10eaed7eceb26da2079530165abf3602d6734225
Viresh Soedhwa <vsoedhwa@bcit.ca>
2020-09-23 12:25:08 -0700

    added database settings

--- qcon/settings.py
+++ qcon/settings.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': database.config()
 }
 
 
